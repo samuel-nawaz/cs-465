161b92dd427cf257b9e204be69ecc8a6927fefe2
tripl <triple7empires@hotmail.com>
2023-07-13 20:05:14 -0400

    Convert travel template from static data to dynamic JSON

---
+++ app_server/controllers/travel.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
+/* GET traveler view */
+const travel = (req, res) => {
+    pageTitle = process.env.npm_package_description + ' - Travel';
+    res.render('travel', {title: pageTitle, trips});
+};
+
+module.exports = {
+    travel
+};
