4834af8b3a9fc05c03067f2aa5f9fb8e899dd54c
tripl <triple7empires@hotmail.com>
2023-07-23 17:58:04 -0400

    convert travel template from static to dynamic JSON

--- app_server/app_api/routes/index.js
+++ app_server/app_api/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const controller = require('../controllers/main');
+const controller = require('../app_api/controllers/main');
 
 /* GET home page. */
 router.get('/', controller.index);
--- app_server/app_api/routes/travel.js
+++ app_server/app_api/routes/travel.js
@@ -1,6 +1,6 @@
 const express = require('express'); 
 const router = express.Router(); 
-const controller= require('../controllers/travel'); 
+const controller= require('../app_api/controllers/travel'); 
 
 /* GET home page. */ 
 router.get('/', controller.travel); 
